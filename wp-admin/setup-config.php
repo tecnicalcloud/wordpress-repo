0ddd4346bf2ad5a024d9d6bc20f7880904afca9d
zohaibashraf <tecnical.cloud@gmail.com>
2021-08-17 20:43:15 +0500

    6th commit

--- wp-admin/setup-config.php
+++ wp-admin/setup-config.php
@@ -163,8 +163,8 @@ switch ( $step ) {
 <h1 class="screen-reader-text"><?php _e( 'Before getting started' ); ?></h1>
 <p><?php _e( 'Welcome to WordPress. Before getting started, we need some information on the database. You will need to know the following items before proceeding.' ); ?></p>
 <ol>
-	<li><?php _e( 'Database name' ); ?></li>
-	<li><?php _e( 'Database username' ); ?></li>
+	<li><?php _e( 'RDS name ' ); ?></li>
+	<li><?php _e( 'RDS username' ); ?></li>
 	<li><?php _e( 'Database password' ); ?></li>
 	<li><?php _e( 'Database host' ); ?></li>
 	<li><?php _e( 'Table prefix (if you want to run more than one WordPress in a single database)' ); ?></li>
