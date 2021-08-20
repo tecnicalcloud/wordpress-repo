361fa1d87d8522384a5963e0b5b9bd4a4a7abd9e
zohaibashraf <tecnical.cloud@gmail.com>
2021-08-20 19:56:21 +0500

    file added

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -10,15 +10,16 @@ sudo yum install -y python-pip
 sudo pip install awscli
 yum update -y
 yum install -y httpd 
+sudo service enable httpd 
+sudo service start httpd 
+sudo yum install git -y 
 sudo sudo yum install -y amazon-linux-extras
 sudo yum install -y amazon-linux-extras
 sudo  amazon-linux-extras | grep php
 sudo amazon-linux-extras enable php7.4 -y
 sudo yum clean metadata -y
 sudo yum install php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap} -y
-sudo service enable httpd 
-sudo service start httpd 
-sudo yum install git -y 
+
 
 
 
